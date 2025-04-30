2e63c96839c0db95d43697bebd29257b3ca2084b
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2025-04-30 15:18:54 +0100

    Fix - Fixed the port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT }, () => {
+const PORT = env.PORT || 3333;
+
+app.listen({ port: PORT }, () => {
 	console.log(`Server is running on port ${env.PORT}`);
 });
