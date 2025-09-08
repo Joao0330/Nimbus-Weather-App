801ee667c96eeb67241c31429bbb39646887c74d
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2025-09-08 11:30:07 +0100

    Style - Changed the loader style

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -4,6 +4,10 @@ export const Loader = () => {
 			<svg viewBox='25 25 50 50'>
 				<circle r='20' cy='50' cx='50'></circle>
 			</svg>
+			<div>
+				<span>Loading...</span>
+				<p>Please be patient, this loading process may take up to 1 minute or more when first visiting the site after a while due to server limitations.</p>
+			</div>
 		</div>
 	);
 };
